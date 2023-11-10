a09af4d25423978ee19c8fe483d442deccd2875b
MrDjeb <MrDjeb245@gmail.com>
2023-11-10 22:50:26 +0300

    add

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp -r /home/ubuntu/filler/photos/holodilniki/* /data/public/static/images/
+cp -r /home/ubuntu/filler/photos/stulya/* /data/public/static/images/
+cp -r /home/ubuntu/filler/photos/divany/* /data/public/static/images/
+cp -r /home/ubuntu/filler/photos/rabochie-stoly/* /data/public/static/images/
+cp -r /home/ubuntu/filler/photos/kresla/* /data/public/static/images/
