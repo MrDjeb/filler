5bb0bcbaf57993eefe88b7098636b7264084594b
MrDjeb <MrDjeb245@gmail.com>
2023-11-11 00:58:56 +0300

    fff

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cp -r /home/ubuntu/filler/photos/holodilniki/* /data/public/static/images/
-cp -r /home/ubuntu/filler/photos/stulya/* /data/public/static/images/
-cp -r /home/ubuntu/filler/photos/divany/* /data/public/static/images/
-cp -r /home/ubuntu/filler/photos/rabochie-stoly/* /data/public/static/images/
-cp -r /home/ubuntu/filler/photos/kresla/* /data/public/static/images/
+cp -r /home/ubuntu/filler/photos/tetradi/* /data/public/static/images/
+cp -r /home/ubuntu/filler/photos/pismennye-prinadlezhnosti/* /data/public/static/images/
+cp -r /home/ubuntu/filler/photos/penaly/* /data/public/static/images/
+cp -r /home/ubuntu/filler/photos/kley-455773/* /data/public/static/images/
