ce20f8e4c808acf7335b7a83e5ef2647ebefdb27
MrDjeb <MrDjeb245@gmail.com>
2023-12-24 22:02:24 +0300

    webp

---
+++ cmd/webp/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"image/jpeg"
+	"log"
+	"os"
+
+	"github.com/kolesa-team/go-webp/encoder"
+	"github.com/kolesa-team/go-webp/webp"
+)
+
+func main() {
+	file, err := os.Open("photos/divany/93-00aadf3e-bf85-439d-a376-78de2b475ffc.jpeg")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	img, err := jpeg.Decode(file)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	output, err := os.Create("photos/93-00aadf3e-bf85-439d-a376-78de2b475ffc.webp")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer output.Close()
+
+	options, err := encoder.NewLossyEncoderOptions(encoder.PresetDefault, 75)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := webp.Encode(output, img, options); err != nil {
+		log.Fatalln(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.3.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/kolesa-team/go-webp v1.0.4 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	golang.org/x/sys v0.14.0 // indirect
 )
