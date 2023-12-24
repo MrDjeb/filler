4055b88c837bdb76afbd47c9a0966b0a40b6ef76
MrDjeb <MrDjeb245@gmail.com>
2023-12-25 02:33:20 +0300

    FIX sql

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@
 cp -r /home/ubuntu/filler/photos/nintendo/* /data/public/static/images/
 cp -r /home/ubuntu/filler/photos/xbox/* /data/public/static/images/
 cp -r /home/ubuntu/filler/photos/playstation/* /data/public/static/images/
+cp -r /home/ubuntu/filler/images/* /data/public/static/images/
